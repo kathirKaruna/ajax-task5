18f57278927ae5506d48a90416a8e0a6c26d937e
Kathiravan Karuna <kathiravankaruna92@gmail.com>
2020-11-02 17:38:37 +0530

    ajax3

---
+++ main.js
@@ -0,0 +1,11 @@
+// var jwtToken = "";
+// var logAgeId = "";
+// $( document ).ready(function() {
+//     var test = JSON.parse(localStorage.getItem("testKey"));
+//    for(i = 0; i < test.length; i++){
+//     var obj = test[i];
+//     jwtToken = obj.token;
+//     logAgeId = obj.agentId; 
+// }
+
+// });
